7a21eae83cf695ab1f69444f76d511b26badf4b4
Sehajroop Singh <freeofborders@gmail.com>
2023-11-29 12:45:53 +0100

    new main

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+import Greet from './components/greet';
+import Welcome from './components/Welcome';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Greet />
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/greet.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// function Greet() {
+//function Greet() {
+//    return <h1>Hello Sehaj</h1>
+//}
+const Greet = () => <h1>Hello Sehaj</h1>
+
+export default Greet;
