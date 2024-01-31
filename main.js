259de4f9267254133cb6a51ba274ecf1fca1da36
Sehajroop Singh <freeofborders@gmail.com>
2024-02-01 00:38:35 +0530

    change to vite

--- main.js
+++ main.js
@@ -1,32 +1,19 @@
-import ForceGraph3D from '3d-force-graph';
-
-const graphData = {
-    "nodes": [
-        {
-          "id": "id1",
-          "name": "name1",
-          "val": 1
-        },
-        {
-          "id": "id2",
-          "name": "name2",
-          "val": 10
-        },
-    ],
-    "links": [
-        {
-            "source": "id1",
-            "target": "id2"
-        },
-    ]
-};
-
-const myGraph = ForceGraph3D();
-myGraph(document.getElementById('3d-graph'))
-  .graphData(graphData);
+import * as THREE from 'https://unpkg.com/three@0.120.1/build/three.module.js'
 
 
-// Add a "Hello" message
-const helloMessage = document.createElement('h1'); // Create a <h1> element
-helloMessage.textContent = 'Hello, welcome to my 3D Force-Directed Graph project!'; // Set its text content
-document.body.insertBefore(helloMessage, document.getElementById('3d-graph')); // Insert it before the graph container
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Fetch the JSON data
+    fetch('./Data/disney_heirarchy.json')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data); // The JSON data is now available here
+        
+        // Assuming ForceGraph3D is globally available, e.g., via a script tag in your HTML
+        const myGraph = ForceGraph3D();
+        
+        // Use the fetched data for your graph
+        myGraph(document.getElementById('3d-graph')).graphData(data);
+      })
+      .catch(error => console.error('Error loading JSON:', error));
+  });
