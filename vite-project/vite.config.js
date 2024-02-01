2a4edc848cd41da593c8bf847d0a6a1593bc3931
Sehajroop Singh <freeofborders@gmail.com>
2024-02-01 09:51:19 +0530

    commit

---
+++ vite-project/vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/SehajroopSingh.github.io/', // Replace 'repository-name' with the name of your GitHub repository
+});
+
+
+
